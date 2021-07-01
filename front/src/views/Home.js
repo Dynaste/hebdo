e82eabbc6a8d9b41e8bbc3f6240af48d54a4a9b1
Dynaste <maximemary.pro@gmail.com>
2021-07-01 12:19:52 +0200

    button login/logout in home view

--- front/src/views/Home.js
+++ front/src/views/Home.js
@@ -1,19 +1,59 @@
 import React from 'react';
 import { useHistory, Redirect } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import { isTokenValid } from '../functions/useApi';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        margin: 'auto',
+        width: '50%',
+        textAlign: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
+    field: {
+        margin: 5,
+    },
+    button: {
+        margin: 5,
+    },
+    link: {
+        margin: 5,
+        color: '#1F85DE',
+        '&:hover': {
+            cursor: 'pointer',
+        },
+    },
+}));
 
 const Home = () => {
     const history = useHistory();
+    const classes = useStyles();
+
+    const log = () => {
+        localStorage.removeItem('token');
+        history.push('/login');
+    }
 
     return (
         <div>
             {isTokenValid() ? (
-                <>
+                <div className={classes.root}>
                     <h2>Home</h2>
 
-                    <p onClick={() => history.push('/login')}>Login</p>
-                    <p onClick={() => history.push('/signup')}>Signup</p>
-                </>
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                        onClick={() => log()}
+                    >
+                        {
+                            isTokenValid() ? 'Logout' : 'Login'
+                        }
+                    </Button>
+                </div>
             ) : (
                 <Redirect to={{ pathname: '/login' }} />
             )}
