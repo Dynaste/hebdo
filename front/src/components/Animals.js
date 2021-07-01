2f5126679bd8841cf83bff3d4248c8396b832972
Dynaste <maximemary.pro@gmail.com>
2021-07-01 17:17:04 +0200

    detailled view article/animal ok

--- front/src/components/Animals.js
+++ front/src/components/Animals.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import { getLink } from '../functions/useApi';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const useStyles = makeStyles(() => ({
     articleContainer: {
@@ -12,8 +19,8 @@ const useStyles = makeStyles(() => ({
         minWidth: 150,
         '&:hover': {
             cursor: 'pointer',
-            opacity: 0.7
-        }
+            opacity: 0.7,
+        },
     },
     container: {
         background: '#f5f5f5',
@@ -26,21 +33,38 @@ const useStyles = makeStyles(() => ({
         justifyContent: 'center',
         flexWrap: 'wrap',
     },
+    contentField: {
+        width: '100%',
+        marginTop: 35,
+    },
 }));
 
 const Animals = ({ animals }) => {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [dialogContent, setDialogContent] = React.useState(false);
+    const [selectedAnimal, setSelectedAnimal] = React.useState(false);
 
-    const newAnimal = () => {
-        console.log('WIP');
+    const handleClickOpen = (type) => {
+        setDialogContent(type);
+        setOpen(true);
     };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const openAnimal = async(item) => {
+        setSelectedAnimal(item);
+        handleClickOpen('GET')
+    }
 
     return (
         <div className={classes.container}>
             <Button
                 variant="contained"
                 color="primary"
-                onClick={() => newAnimal()}
+                onClick={() => handleClickOpen('POST')}
             >
                 Ajouter un animal
             </Button>
@@ -52,18 +76,80 @@ const Animals = ({ animals }) => {
                                 boxShadow={1}
                                 className={classes.articleContainer}
                                 key={key}
+                                onClick={() => openAnimal(item)}
                             >
                                 <p>{item.name}</p>
-                                <p>{item.type}</p>
-                                <p>{item.race}</p>
-                                <p>{item.age}</p>
-                                <p>{item.weight}</p>
+                                <p>Type: {item.type}</p>
+                                <p>Race: {item.race}</p>
+                                {/* <p>{item.age}</p>
+                                <p>{item.weight}</p> */}
                             </Box>
                         )
                 )}
             </div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="form-dialog-title"
+            >
+                {dialogContent === 'POST' && (
+                    <>
+                        <DialogTitle id="form-dialog-title">
+                            Ajout d'un animal
+                        </DialogTitle>
+                        <DialogContent>
+                            <TextField
+                                margin="dense"
+                                id="name"
+                                label="Titre"
+                                fullWidth
+                            />
+                            <TextField
+                                margin="dense"
+                                id="name"
+                                label="Sous-titre"
+                                fullWidth
+                            />
+                            <TextareaAutosize
+                                aria-label="minimum height"
+                                rowsMin={10}
+                                placeholder="Contenu de l'article"
+                                className={classes.contentField}
+                            />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">
+                                Annuler
+                            </Button>
+                            <Button onClick={handleClose} color="primary">
+                                Envoyer
+                            </Button>
+                        </DialogActions>
+                    </>
+                )}
+                {dialogContent === 'GET' && (
+                    <>
+                        <DialogTitle id="form-dialog-title">
+                            {selectedAnimal.name}
+                        </DialogTitle>
+                        <DialogContent style={{textAlign: 'center'}}>
+                            <h4>Type: {selectedAnimal.type}</h4>
+                            <h4>Race: {selectedAnimal.race}</h4>
+                            <p>Poids: {selectedAnimal.weight}</p>
+                            <p>Age: {selectedAnimal.age}</p>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">
+                                Fermer
+                            </Button>
+                        </DialogActions>
+                    </>
+                )}
+            </Dialog>
         </div>
     );
 };
 
 export default Animals;
+
+
