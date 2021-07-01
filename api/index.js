69151ace07d3700496c69b71e0f48d9b6a3705b3
Dynaste <maximemary.pro@gmail.com>
2021-07-01 09:03:32 +0200

    change path of import in index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
-const {port, baseUrl: hostname} = require('./api/config');
+const {port, baseUrl: hostname} = require('./config');
 const {DB_USERNAME, DB_PASSWORD, DB_TABLE} = process.env;
 // const dbUrl = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.5yp3u.mongodb.net/${DB_TABLE}?retryWrites=true&w=majority`;
 const dbUrl = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@cluster0-shard-00-00.5yp3u.mongodb.net:27017,cluster0-shard-00-01.5yp3u.mongodb.net:27017,cluster0-shard-00-02.5yp3u.mongodb.net:27017/${DB_TABLE}?ssl=true&replicaSet=atlas-k3o6xi-shard-0&authSource=admin&retryWrites=true&w=majority`;
@@ -25,11 +25,11 @@ mongoose
     });
 
 
-const animalRoutes = require('./api/routes/animalRoutes');
+const animalRoutes = require('./routes/animalRoutes');
 animalRoutes(app);
-const userRoutes = require('./api/routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
 userRoutes(app);
-// const articleRoutes = require('./api/routes/articleRoutes');
+// const articleRoutes = require('./routes/articleRoutes');
 // articleRoutes(app);
 
 
